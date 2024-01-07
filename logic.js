ef66d8dcbf0669554d6b8287db90d89c6cd492c1
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-07 20:31:20 +0100

    Finish Password matching JS logic

--- logic.js
+++ logic.js
@@ -2,16 +2,6 @@ const password1 = document.querySelector("#password");
 const password2 = document.querySelector("#password-confirm");
 const button = document.querySelector("button");
 const noMatch = document.querySelector("#no-match");
-const inputs = document.querySelectorAll("input");
-
-
-inputs.forEach(e => {
-    e.addEventListener("input", () => {
-        if(e.value.length == 0) {
-            e.value = "";
-        }
-    })
-});
 
 button.addEventListener("click", checkPasswords);
 
@@ -21,5 +11,11 @@ function checkPasswords () {
         noMatch.style.visibility = "visible";
         password1.style.borderColor = "rgb(150, 8, 8)";
         password2.style.borderColor = "rgb(150, 8, 8)";
+        password1.setCustomValidity(" ");
+
+    }
+    else {
+        password1.setCustomValidity("");
+        password2.setCustomValidity("");
     }
 };
