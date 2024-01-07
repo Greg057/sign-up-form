caf249bc9cc60c83d7e4aca7ea656553d60368be
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-07 20:14:06 +0100

    Add input validation

--- logic.js
+++ logic.js
@@ -1,16 +1,25 @@
 const password1 = document.querySelector("#password");
 const password2 = document.querySelector("#password-confirm");
 const button = document.querySelector("button");
+const noMatch = document.querySelector("#no-match");
+const inputs = document.querySelectorAll("input");
 
+
+inputs.forEach(e => {
+    e.addEventListener("input", () => {
+        if(e.value.length == 0) {
+            e.value = "";
+        }
+    })
+});
 
 button.addEventListener("click", checkPasswords);
 
 
 function checkPasswords () {
     if (password1.value != password2.value) {
-        console.log("nope");
-    }
-    else {
-        console.log("all good");
+        noMatch.style.visibility = "visible";
+        password1.style.borderColor = "rgb(150, 8, 8)";
+        password2.style.borderColor = "rgb(150, 8, 8)";
     }
 };
